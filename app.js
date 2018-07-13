a781180e68adf737e9af20d0820a80e309bc1324
Tyler Bray <broncobray77@gmail.com>
2018-07-13 12:37:53 -0700

    backend setup

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const nodemailer = require('nodemailer');
+
+const app = express();
+
+// View engine setup
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// Static folder
+app.use('/views', express.static(path.join(__dirname, 'views')));
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.render('contact');
+});
+
+
+app.listen(3000, () => console.log('Server started...'));
